08009fc10dcad94d65293477e2f4857ccd4fbb9e
Chris Collier <czcollier@gmail.com>
2013-04-03 22:45:18 -0400

    initial creat

---
+++ src/main/scala/com/bullhorn/JPAQuery.scala
@@ -0,0 +1,58 @@
+package com.bullhorn
+
+import Query._
+
+class JPAQuery[T](q: Query[T]) {
+  val max = q.max
+  val typeName = q.from.typeName
+  val query = "FROM %s WHERE %s".format(q.from.typeName, JPAQuery.emitExpr(new ExprCount, q.where))
+
+  println("building query: %s".format(query))
+
+  val params = JPAQuery.setParameters(Map(), new ExprCount, q.where)
+  println("with parameters:")
+  for (p <- params) { println("%s -> %s".format(p._1, p._2.toString)) }
+}
+
+class ExprCount {
+  private var cnt = 0
+  def ++ = {
+    cnt += 1
+    this
+  }
+
+  def apply() = "param_%s".format(cnt)
+}
+
+object JPAQuery {
+  private def formatCond(cnt: ExprCount, name: String, op: String) = "%1$s %2$s :%3$s".format(name, op, cnt++())
+
+  private def emitExpr(cnt: ExprCount, e: Expr): String = {
+    e match {
+      case x:EqualsCondition => formatCond(cnt, x.name, "=")
+      case x:LikeCondition => formatCond(cnt, x.name, "LIKE")
+      case x:GTCondition[_] => formatCond(cnt, x.name, ">")
+      case x:LTCondition[_] => formatCond(cnt, x.name, "<")
+      case x:AndOp => emitExpr(cnt, x.left) + " AND " + emitExpr(cnt, x.right)
+      case x:OrOp => emitExpr(cnt, x.left) + " OR " + emitExpr(cnt, x.right)
+    }
+  }
+
+  private def formatValue(cond: Condition) =
+    cond match {
+      case x:LikeLeftCondition => "%%%s".format(cond.value.value)
+      case x:LikeRightCondition => "%s%%".format(cond.value.value)
+      case _ =>  cond.value.value
+    }
+
+  private def setParameters(map: Map[String, Any], cnt: ExprCount, e: WhereExpr): Map[String, Any] = {
+    e match {
+      case x:Condition => map + (cnt++() -> formatValue(x))
+      case x:BooleanOp => {
+        setParameters(map, cnt,  x.bl) ++ setParameters(map, cnt,  x.br)
+      }
+    }
+  }
+
+  implicit def query2JPAQuery[T](q: Query[T]) = new JPAQuery(q)
+}
---
+++ src/main/scala/com/bullhorn/Query.scala
@@ -0,0 +1,56 @@
+package com.bullhorn
+
+abstract class Expr
+abstract class BooleanExpr(val left: Expr, val right: Expr) extends Expr
+
+class Query[T](val from: FromExpr[T], val where: WhereExpr) extends BooleanExpr(from, where) {
+  private var maxResults = 100
+
+  def max = maxResults
+
+  def max(m: Int) = {
+    this.maxResults = m
+    this
+  }
+}
+
+class FromExpr[T](typeManifest: Manifest[T]) extends Expr {
+  lazy val typeName = typeManifest.erasure.getSimpleName
+  def where(e: WhereExpr) = new Query(this, e)
+}
+
+abstract class WhereExpr(val l: Expr, val r: Expr) extends BooleanExpr(l, r) {
+  def and(right: WhereExpr) = AndOp(this, right)
+  def or(right: WhereExpr) = OrOp(this, right)
+}
+
+abstract class Condition(val name: FieldName, val value: Value[_]) extends WhereExpr(name, value)
+
+abstract class BooleanOp(val bl: WhereExpr, val br: WhereExpr) extends WhereExpr(bl, br)
+
+case class AndOp(andl: WhereExpr, andr: WhereExpr) extends BooleanOp(andl, andr)
+case class OrOp(orl: WhereExpr, orr: WhereExpr) extends BooleanOp(orl, orr)
+
+case class FieldName(name: String) extends Expr {
+  def ===(value: Any) = EqualsCondition(this, Value(value))
+  def #==(value: String) = LikeLeftCondition(this, Value(value))
+  def ==~(value: String) = LikeRightCondition(this, Value(value))
+  def >==[T <: Comparable[T]](value: T) = GTCondition[T](this, Value(value))
+  def <==[T <: Comparable[T]](value: T) = LTCondition[T](this, Value(value))
+}
+
+case class Value[T](value: T) extends Expr
+
+abstract class LikeCondition(n: FieldName, v: Value[String]) extends Condition(n, v)
+case class EqualsCondition(n: FieldName, v: Value[Any]) extends Condition(n, v)
+case class LikeLeftCondition(n: FieldName, v: Value[String]) extends LikeCondition(n, v)
+case class LikeRightCondition(n: FieldName, v: Value[String]) extends LikeCondition(n, v)
+case class GTCondition[T](n: FieldName, v: Value[Comparable[T]]) extends Condition(n, v)
+case class LTCondition[T](n: FieldName, v: Value[Comparable[T]]) extends Condition(n, v)
+
+object Query {
+  def from[T](implicit manifest: Manifest[T]) = new FromExpr[T](manifest)
+
+  implicit def string2fieldName(name: String) = FieldName(name)
+  implicit def fieldName2String(fn: FieldName) = fn.name
+}
